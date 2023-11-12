af2d459271af38d4ba362452fd3bbd731e4a9231
Griko Nibras <griko@nibras.co>
2023-11-12 23:06:46 +0700

    refactor: rework redirect middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@vercel/edge-config";
 import { StatusCodes } from "http-status-codes";
 import { NextRequest, NextResponse } from "next/server";
+import { ignoreAssert, ignoreNullable, ignoreSymbol } from "./utils/assert";
 
 export const config = {
   matcher: [
@@ -11,15 +12,19 @@ export const config = {
 };
 
 export async function middleware(req: NextRequest) {
-  if (process.env.NEXT_PUBLIC_REDIRECT_HOST && process.env.REDIRECT_EDGE_CONFIG) {
-    if (!req.headers.get("host")?.endsWith(process.env.NEXT_PUBLIC_REDIRECT_HOST)) return;
+  try {
+    const host = ignoreNullable(req.headers.get("host"));
+    const redirectHost = ignoreNullable(process.env.NEXT_PUBLIC_REDIRECT_HOST);
+    ignoreAssert(host.includes(redirectHost));
 
-    const slug = req.nextUrl.pathname.split("/")[1];
-    if (!slug) return;
-
-    const url = await createClient(process.env.REDIRECT_EDGE_CONFIG).get(slug);
-    if (!(typeof url === "string" && url)) return;
+    const edgeConfig = ignoreNullable(process.env.REDIRECT_EDGE_CONFIG);
+    const slug = ignoreNullable(req.nextUrl.pathname.split("/")[1]);
+    const url = await createClient(edgeConfig).get(slug);
+    ignoreAssert(typeof url === "string" && url);
 
     return NextResponse.redirect(url, { status: StatusCodes.TEMPORARY_REDIRECT });
+  } catch (error) {
+    if (error === ignoreSymbol) return;
+    console.error(error);
   }
 }
