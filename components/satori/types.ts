85d40c38ad8d0e05791b91335bad58ff4a3ff8b5
Griko Nibras <griko@nibras.co>
2023-11-13 01:20:12 +0700

    feat: improve satori typings

--- components/satori/types.ts
+++ components/satori/types.ts
@@ -1,23 +1,40 @@
 import { defaultMetadata } from "@/site.config";
 import { toSearchString } from "@/utils/string";
+import { z } from "zod";
 
-export interface ArticleLayoutProps {
-  title: string;
-  description: string;
-  imgSrc?: string;
-  path?: string;
-}
+/* #__PURE__ */
+export const articleLayoutSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  imgSrc: z.string().optional(),
+  path: z.string().optional(),
+});
 
-export function getArticleLayoutSearchParams(sp: URLSearchParams): ArticleLayoutProps {
+export type ArticleLayoutProps = z.infer<typeof articleLayoutSchema>;
+
+export function getArticleLayoutSearchParams(usp: URLSearchParams): ArticleLayoutProps {
+  const encoded = usp.get("encoded");
+  if (encoded) {
+    try {
+      const decoded = Buffer.from(encoded, "base64").toString("utf-8");
+      const parsed = articleLayoutSchema.parse(JSON.parse(decoded));
+      return parsed;
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return {
-    title: sp.get("title") || defaultMetadata.title,
-    description: sp.get("description") || defaultMetadata.description,
-    imgSrc: sp.get("imgSrc") || undefined,
-    path: sp.get("path") || undefined,
+    title: usp.get("title") || defaultMetadata.title,
+    description: usp.get("description") || defaultMetadata.description,
+    imgSrc: usp.get("imgSrc") || undefined,
+    path: usp.get("path") || undefined,
   };
 }
 
-export function getArticleLayoutSearchString(props: ArticleLayoutProps): string {
+export function getArticleLayoutSearchString(props: ArticleLayoutProps, { encoded = false } = {}): string {
   const { title, description, imgSrc = "", path = "" } = props;
+  if (encoded) {
+    return Buffer.from(JSON.stringify({ title, description, imgSrc, path })).toString("base64");
+  }
   return toSearchString({ title, description, imgSrc, path });
 }
