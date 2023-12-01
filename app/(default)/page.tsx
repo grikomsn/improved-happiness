d5d8be83b84d8505ac0dba40bbe42051a33376ca
Griko Nibras <griko@nibras.co>
2023-12-01 14:00:10 +0700

    feat: update home page

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -3,17 +3,22 @@ import { ContainerSection } from "@/components/container-section";
 import { getBasicRenderers } from "@/components/keystatic/basic-renderers";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 import { links } from "@/constants/links";
-import { getReader } from "@/server/keystatic";
+import { getHomeSingleton, getSortedArticles } from "@/server/keystatic";
 import { defaultMetadata } from "@/site.config";
 import { cn } from "@/utils/ui";
 import { DocumentRenderer } from "@keystatic/core/renderer";
+import { ArrowRightIcon } from "lucide-react";
+import Link from "next/link";
 
 export default async function Page() {
-  const query = await getReader().singletons.home.readOrThrow();
+  const query = await getHomeSingleton();
   const content = await query.content();
   const renderers = getBasicRenderers();
+
+  const [firstArticle] = await getSortedArticles();
+
   return (
-    <ContainerSection className="relative">
+    <ContainerSection className="relative flex flex-col items-start">
       <h1 className="mb-2 text-4xl font-bold">{defaultMetadata.title}</h1>
       <p className="mb-8 text-2xl">{defaultMetadata.description}</p>
       <div
@@ -48,6 +53,31 @@ export default async function Page() {
           </li>
         ))}
       </ul>
+      {firstArticle && (
+        <Link
+          href={`/articles/${firstArticle.slug}`}
+          className={cn(
+            "flex items-center space-x-2 rounded-full px-2 py-1 pr-3 text-sm",
+            "-order-1 mb-8",
+            "border border-neutral-500/25 hover:border-neutral-500/50",
+            "bg-neutral-500/10",
+            "text-neutral-700 dark:text-neutral-300",
+          )}
+        >
+          <span
+            className={cn(
+              "rounded-full px-2 py-px",
+              "text-xs font-semibold text-neutral-200",
+              "bg-primary-500/50",
+              //
+            )}
+          >
+            New Article
+          </span>
+          <span>{firstArticle.entry.title}</span>
+          <ArrowRightIcon className="h-3 w-3" />
+        </Link>
+      )}
     </ContainerSection>
   );
 }
