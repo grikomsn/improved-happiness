91c76aa7eeb463d638b9a609235bbbb58462d40a
Griko Nibras <griko@nibras.co>
2024-03-20 23:12:40 +0700

    fix: update lenis config

--- app/(default)/providers.tsx
+++ app/(default)/providers.tsx
@@ -87,7 +87,6 @@ function RegisterLenis() {
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
       gestureOrientation: "vertical",
       infinite: false,
-      normalizeWheel: true,
       orientation: "vertical",
       smoothWheel: true,
       syncTouch: true,
