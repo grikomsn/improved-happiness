7e3545de0d88fb480787eddc146903e3dc874967
Griko Nibras <griko@nibras.co>
2024-04-12 02:39:12 +0800

    feat: update icons

--- constants/links.ts
+++ constants/links.ts
@@ -1,21 +1,23 @@
+import { BlueskyIcon } from "@/components/icons/bluesky";
 import { CohostIcon } from "@/components/icons/cohost";
+import { GitHubIcon } from "@/components/icons/github";
 import { KeybaseIcon } from "@/components/icons/keybase";
 import { MastodonIcon } from "@/components/icons/mastodon";
 import { MatrixIcon } from "@/components/icons/matrix";
 import { XIcon } from "@/components/icons/x";
 import { customMetadata } from "@/site.config";
-import { CloudIcon, GithubIcon, LinkedinIcon } from "lucide-react";
+import { LinkedinIcon } from "lucide-react";
 
 export const links = [
-  { href: customMetadata.githubUrl, Icon: GithubIcon, label: "GitHub" },
+  { href: customMetadata.githubUrl, Icon: GitHubIcon, label: "GitHub" },
   { href: customMetadata.linkedInUrl, Icon: LinkedinIcon, label: "LinkedIn" },
   { href: customMetadata.xUrl, Icon: XIcon, label: "X (formerly Twitter)" },
 ];
 
 export const wholeLinks = [
-  { href: customMetadata.blueskyUrl, Icon: CloudIcon, label: "Bluesky" },
+  { href: customMetadata.blueskyUrl, Icon: BlueskyIcon, label: "Bluesky" },
   { href: customMetadata.cohostUrl, Icon: CohostIcon, label: "Cohost" },
-  { href: customMetadata.githubUrl, Icon: GithubIcon, label: "GitHub" },
+  { href: customMetadata.githubUrl, Icon: GitHubIcon, label: "GitHub" },
   { href: customMetadata.linkedInUrl, Icon: LinkedinIcon, label: "LinkedIn" },
   { href: customMetadata.keybaseUrl, Icon: KeybaseIcon, label: "Keybase" },
   { href: customMetadata.mastodonUrl, Icon: MastodonIcon, label: "Mastodon" },
