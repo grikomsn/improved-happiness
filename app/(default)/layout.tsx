9541a5d4f3959b955ee556ed7b87248836dc478b
Griko Nibras <griko@nibras.co>
2024-04-12 03:22:36 +0800

    feat: layout y paddings

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -13,7 +13,7 @@ export default function Layout({ children }: { children: ReactNode }) {
       <Navbar />
       <main
         className={cn(
-          "pb-16",
+          "py-16",
           "container relative isolate max-w-screen-md flex-grow",
           "before:pointer-events-none before:fixed before:inset-x-0 before:bottom-0 before:z-10 before:h-8 before:max-md:hidden",
           "before:bg-gradient-to-t",
