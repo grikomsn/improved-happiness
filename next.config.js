fab2d3e4f96d69d95261a20ea84e5e98cbf1e54f
Griko Nibras <griko@nibras.co>
2024-04-19 18:21:56 +0700

    feat: add csp headers

--- next.config.js
+++ next.config.js
@@ -5,6 +5,24 @@ const APP_URL =
   (process.env.VERCEL && `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`) ||
   `${process.env.PROTOCOL || "http"}://${process.env.HOST || "localhost"}:${process.env.PORT || 3000}`;
 
+const domain =
+  APP_URL?.replace(/https?:\/\//, "").split("/")[0] ||
+  process.env.NEXT_PUBLIC_VERCEL_URL ||
+  `${process.env.HOST || "localhost"}:${process.env.PORT || 3000}`;
+
+const CSP_HEADER = `
+  child-src *.twitter.com;
+  connect-src *;
+  default-src 'self';
+  font-src data: 'self' *.${domain} fonts.gstatic.com;
+  img-src * blob: data:;
+  media-src 'none';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.${domain} *.vercel-scripts.com fonts.googleapis.com;
+  style-src 'self' 'unsafe-inline' *.${domain} fonts.googleapis.com;
+`
+  .replace(/^\s+/g, "")
+  .trim();
+
 /**
  * @type {import("next").NextConfig}
  * @see https://nextjs.org/docs/app/api-reference/next-config-js
@@ -13,6 +31,20 @@ const nextConfig = {
   env: {
     APP_URL,
   },
+  headers: async () => [
+    {
+      source: "/(.*)",
+      headers: [
+        { key: "Content-Security-Policy", value: CSP_HEADER.replace(/\n/g, "") },
+        { key: "Permissions-Policy", value: "camera=(), geolocation=(), interest-cohort=(), microphone=()" },
+        { key: "Referrer-Policy", value: "origin-when-cross-origin" },
+        { key: "Strict-Transport-Security", value: "max-age=31536000; includeSubDomains; preload" },
+        { key: "X-Frame-Options", value: "DENY" },
+        { key: "X-Content-Type-Options", value: "nosniff" },
+        { key: "X-DNS-Prefetch-Control", value: "on" },
+      ],
+    },
+  ],
   reactStrictMode: true,
   redirects: async () => [
     { source: "/about-me", destination: "/about", permanent: true },
